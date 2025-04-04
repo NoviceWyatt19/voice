2ef4a86e6ceef5a4f2aa532c47352f4ae7d74164
Wyatt <155226507+NoviceWyatt19@users.noreply.github.com>
2025-04-04 14:36:42 +0900

    Update kakaoBook.js

--- assets/js/module/kakaoBook.js
+++ assets/js/module/kakaoBook.js
@@ -1,9 +1,5 @@
-// js : 8046adfd370bf95f54d67e01ccb119e3
-// rest : 08b3d148ae311820628f053372d71dd4
-// https://dapi.kakao.com/v3/search/book
-// KakaoAK 08b3d148ae311820628f053372d71dd4
 const API_URL = 'https://dapi.kakao.com/v3/search/book?';
-const API_KEY = 'KakaoAK 08b3d148ae311820628f053372d71dd4';
+const API_KEY = 'KakaoAK rest-api-key';
 
 const api = async params => {
   const queryString = new URLSearchParams(params).toString();
